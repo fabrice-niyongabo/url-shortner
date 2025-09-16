93af748597f49d77476e1bea5d15c0122a07c9bb
Fabrice Niyongabo <ninga.fab@gmail.com>
2025-09-16 08:18:41 +0200

    fix edge runtime

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,10 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
 export default prisma;
